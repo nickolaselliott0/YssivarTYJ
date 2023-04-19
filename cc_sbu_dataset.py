dec2fc27858f549235176e4315057fb65e677319
nickolaselliott0 <104137771+nickolaselliott0@users.noreply.github.com>
2023-04-19 21:20:23 +0300

    Update cc_sbu_dataset.py

--- cc_sbu_dataset.py
+++ cc_sbu_dataset.py
@@ -2,7 +2,6 @@ import os
 from PIL import Image
 import webdataset as wds
 from minigpt4.datasets.datasets.base_dataset import BaseDataset
-from minigpt4.datasets.datasets.caption_datasets import CaptionDataset
 
 
 class CCSBUDataset(BaseDataset):
@@ -14,7 +13,6 @@ class CCSBUDataset(BaseDataset):
             wds.tarfile_to_samples(handler=wds.warn_and_continue),
             wds.shuffle(1000, handler=wds.warn_and_continue),
             wds.decode("pilrgb", handler=wds.warn_and_continue),
-            wds.to_tuple("jpg", "json", handler=wds.warn_and_continue),
             wds.map_tuple(self.vis_processor, handler=wds.warn_and_continue),
             wds.map(self.to_dict, handler=wds.warn_and_continue),
         )
